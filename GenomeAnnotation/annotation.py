279fc5d2813fc95a02f052b3642863c81059fd9e
Recep Can Altınbağ <recepcanaltinbag@gmail.com>
2019-09-18 11:52:04 +0300

    smart cutter due to features function is added
    some bugs are fixed

--- GenomeAnnotation/annotation.py
+++ GenomeAnnotation/annotation.py
@@ -12,7 +12,7 @@ import PySimpleGUI as sg
 import os
 from BCBio import GFF
 
-cut_number = 15000
+cut_number = 12000
 
 start_the_program = False
 
@@ -46,8 +46,8 @@ list_contigs.reverse()
 layout = [
     [sg.Text('Pick a Cut Number(between 10000-15000 is ideal)'
              '\nIf DNA segment is bigger than cut number, it will divide to parts'
-             '{\nor you can write a specific portion of the contig/sequence')],
-               [sg.InputText('12000'),sg.Checkbox('Specific part of the DNA (ex: 12345-12678)')],
+             '{\nor you can write a specific portion of the contig/sequence (ex: 12345-12678)')],
+               [sg.InputText('12000')],
                [sg.Text('Pick a Contig/DNA Segment')], [sg.Listbox(values=list_contigs, size=(50, 20))],
                [sg.Submit(), sg.Text('created by Rca', size = (50,1), justification='right')] ]
 
@@ -56,41 +56,49 @@ window = sg.Window('Annotation Viewer version 0.0').Layout(layout)
 
 terminate = False
 
+def smart_cutter_function(start,array):
+
+    min = 100000
+    value = 0
+    final_start = 0
+    for element in array:
+        if abs(start-element) < min:
+            min = abs(start-element)
+            final_start = element
+
+    return final_start
+
+
 while terminate == False:
 
+    smart_cutter = []
+    dna_portion = False
     button, values = window.Read()
-    if values[0]==None or values[2]==None:
+
+    if values[0]==None or values[1]==None:
         exit(1)
 
-    print(values)
-
-    portion_dna = values[1]
-    if portion_dna == False:
+    if len(values[0].split('-')) <= 1:
         cut_number = int(values[0])
     else:
         start_char = int(values[0].split('-')[0])
         end_char = int(values[0].split('-')[1])
+        dna_portion = True
         print(start_char)
         print(end_char)
-    sequence_name = str(values[2][0])
+    sequence_name = str(values[1][0])
     print(sequence_name)
 
 
     in_handle = open(in_file)
-
-
     limit_info = dict(
         gff_id = [sequence_name]
     )
 
     output_file_name = sequence_name
-
-
     path = sequence_name
     if not os.path.exists(path):
         os.mkdir(path)
-
-
 
     for rec in GFF.parse(in_handle, limit_info=limit_info):
         if len(rec.features) > 0:
@@ -106,6 +114,11 @@ while terminate == False:
                     set_color = "#ff0000"
 
                 print(str(feature.qualifiers["product"]),1)
+
+                smart_cutter.append(feature.location.start)
+                smart_cutter.append(feature.location.end)
+
+
                 features.append(GraphicFeature(start=feature.location.start,
                                end=feature.location.end,
                                strand=feature.location.strand,
@@ -117,37 +130,30 @@ while terminate == False:
 
             print(float(len(rec.seq)/cut_number))
 
-            if portion_dna == True:
-                print('yey')
-                if end_char<=len(rec.seq):
-                    print('yey')
-                    zoom_start, zoom_end = start_char, end_char
-                    part = str(zoom_start) + 'to' + str(zoom_end)
-                    output_file_name = path + '/' + sequence_name + part + '.png'
-                    cropped_record = record.crop((zoom_start, zoom_end))
-                    ax, _ = cropped_record.plot(figure_width=8)
-                    ax.figure.savefig(output_file_name, bbox_inches='tight')
 
+            element = 0
+            if int(len(rec.seq)) > cut_number and float((len(rec.seq)-element)/cut_number) > 1.3:
+
+                if dna_portion == True:
+                    element = smart_cutter_function(start_char,smart_cutter)
+                    lentgh_of = smart_cutter_function(end_char,smart_cutter)
                 else:
-                    popup = "Enter a portion less than segment length"
-                    sg.Popup(popup)
-                    continue
+                    element = 0
+                    lentgh_of = int(len(rec.seq))
 
-
-            elif int(len(rec.seq)) > cut_number and float(len(rec.seq)/cut_number) > 1.3:
-                element = 0
-                while element < int(len(rec.seq)):
+                while element < lentgh_of:
                     part = str(element)
-                    zoom_start, zoom_end = element, element+cut_number
+                    zoom_start, zoom_end = element, smart_cutter_function(element + cut_number, smart_cutter)
                     print(element)
-                    if element + cut_number > len(rec.seq):
-                        zoom_end = len(rec.seq) - 1
-                        element = len(rec.seq)
+                    if element + cut_number > lentgh_of:
+                        zoom_end = lentgh_of - 1
+                        element = lentgh_of
                     else:
-                        element = element + cut_number
-                        if float(len(rec.seq)/element) < 1.3:
+                        element = smart_cutter_function(element + cut_number, smart_cutter)
+                        if float((len(rec.seq)-element)/cut_number) < 1.3:
                             zoom_end = len(rec.seq) - 1
                             element = len(rec.seq)
+
 
                     part = 'from' + part + 'to' + str(element)
                     output_file_name = path + '/' + sequence_name + part + '.png'
@@ -166,6 +172,9 @@ while terminate == False:
     in_handle.close()
     popup = "A file is created in working folder named as:  " + output_file_name
     sg.Popup(popup)
+
+
+
 
 
 
